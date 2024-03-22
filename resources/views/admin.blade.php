a1366db997613b772a97bf414248c125a348685b
samaneh <samanehmostafazadeh308@gmail.com>
2024-03-22 21:57:53 +0330

    add some css to admin page

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -9,21 +9,21 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <table>
+                    <table class="w-full whitespace-no-wrapw-full whitespace-no-wrap">
                         <thead>
-                        <tr>
-                            <td>Name</td>
-                            <td>Email</td>
-                            <td>Status</td>
-                            <td>link</td>
+                        <tr class="text-center font-bold">
+                            <td class="border px-6 py-4">Name</td>
+                            <td class="border px-6 py-4">Email</td>
+                            <td class="border px-6 py-4">Status</td>
+                            <td class="border px-6 py-4">link</td>
                         </tr>
                         </thead>
                         @foreach($users as $user)
                             <tr>
-                                <td>{{$user->name}}</td>
-                                <td>{{$user->email}}</td>
-                                <td> active </td>
-                                <td> click to edit</td>
+                                <td class="border px-6 py-4">{{$user->name}}</td>
+                                <td class="border px-6 py-4">{{$user->email}}</td>
+                                <td class="border px-6 py-4"> active </td>
+                                <td class="border px-6 py-4"> click to edit</td>
                             </tr>
                         @endforeach
                     </table>
