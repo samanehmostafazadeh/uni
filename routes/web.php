e4a71027bb448c3c86a7be9aab22e9b89267a998
samaneh <samanehmostafazadeh308@gmail.com>
2024-03-22 21:54:13 +0330

    pass users to admin blade

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,7 +13,8 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/admin', function () {
-    return view('admin');
+    $users = User::all();
+    return view('admin',compact('users'));
 })->middleware(['auth', 'verified'])->name('admin');
 
 
