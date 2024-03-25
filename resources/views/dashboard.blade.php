182c119fd6e4478e7b35caf60d4fef555f16a0fb
samaneh <samanehmostafazadeh308@gmail.com>
2024-03-25 20:16:21 +0330

    separate index of posts from dashboard blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,24 +16,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}, My Blog Posts are:
+                    {{ __("You're logged in!") }}
                 </div>
             </div>
         </div>
     </div>
-
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="grid grid-cols-3 gap-4">
-                @foreach($posts as $post)
-                    <div class="...">
-                        <img src="{{ $post->img }}" alt="{{ $post->title}}" />
-                        <a href="#"> {{ $post->title }}</a>
-                    </div>
-                @endforeach
-            </div>
-        </div>
-    </div>
+    @include('post.partials.index-post-gride-show', ['posts' => $posts])
 </x-app-layout>
 
---
+++ resources/views/post/partials/index-post-gride-show.blade.php
@@ -0,0 +1,14 @@
+<section>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="grid grid-cols-3 gap-4">
+                @foreach($posts as $post)
+                    <div class="...">
+                        <img src="{{ $post->img }}" alt="{{ $post->title}}" />
+                        <a href="#"> {{ $post->title }}</a>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</section>
