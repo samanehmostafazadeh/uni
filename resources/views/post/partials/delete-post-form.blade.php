77e52bbb552e125c8d717339e21da6179c378aac
samaneh <samanehmostafazadeh308@gmail.com>
2024-03-25 20:26:57 +0330

    update post resources

--- resources/views/post/partials/delete-post-form.blade.php
+++ resources/views/post/partials/delete-post-form.blade.php
@@ -1,30 +1,30 @@
 <section class="space-y-6">
     <header>
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-            {{ __('Delete Account') }}
+            {{ __('Delete Post') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
+            {{ __('Once your post is deleted, all of its resources and data will be permanently deleted. Before deleting your post, please download any data or information that you wish to retain.') }}
         </p>
     </header>
 
     <x-danger-button
         x-data=""
-        x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
-    >{{ __('Delete Account') }}</x-danger-button>
+        x-on:click.prevent="$dispatch('open-modal', 'confirm-post-deletion')"
+    >{{ __('Delete post') }}</x-danger-button>
 
-    <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
-        <form method="post" action="{{ route('profile.destroy',$user) }}" class="p-6">
+    <x-modal name="confirm-post-deletion" :show="$errors->postDeletion->isNotEmpty()" focusable>
+        <form method="post" action="{{ route('post.destroy',$post) }}" class="p-6">
             @csrf
             @method('delete')
 
             <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                {{ __('Are you sure you want to delete your account?') }}
+                {{ __('Are you sure you want to delete your post?') }}
             </h2>
 
             <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+                {{ __('Once your post is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your post.') }}
             </p>
 
             <div class="mt-6">
@@ -38,7 +38,7 @@
                     placeholder="{{ __('Password') }}"
                 />
 
-                <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
+                <x-input-error :messages="$errors->postDeletion->get('password')" class="mt-2" />
             </div>
 
             <div class="mt-6 flex justify-end">
@@ -47,7 +47,7 @@
                 </x-secondary-button>
 
                 <x-danger-button class="ms-3">
-                    {{ __('Delete Account') }}
+                    {{ __('Delete Post') }}
                 </x-danger-button>
             </div>
         </form>
