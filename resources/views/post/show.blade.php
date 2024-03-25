0aab55dcc2199549e7484e33e838bf21d30479ca
samaneh <samanehmostafazadeh308@gmail.com>
2024-03-26 00:55:36 +0330

    add comment front

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout :user="$user->id">
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Post: ') }} {{ $post->title }}
+            {{ $post->title }}
         </h2>
         <p> {{ __('by: '). $post->user->name }} </p>
     </x-slot>
